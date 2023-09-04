68e0385609f58900dececed28a8cd9e387efca17
Yuji-Hosaka <h.kyouiku58@gmail.com>
2023-09-04 18:46:45 +0700

    Done simple css form

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'; // for create UI
+import ReactDOM from 'react-dom/client'; // for Render
+import './index.css'; // for styling
 
+// Form Component
+//TASK : Create HTML
+function Form() {
+    return <div className='container'>
+      <form className='form'>
+        <div className='form-input'>
+          <label>username</label>
+          <input type='text'/>
+          <small>Identify username at least 8 charecters</small>
+        </div>
+        {/* #2 Email*/}
+        <div className='form-input'>
+          <label>email</label>
+          <input type='email'/>
+          <small>Email pattern is not correct</small>
+        </div>
+
+        {/* #3 : Password */}
+        <div className= 'form-input'>
+          <label>password</label>
+          <input type='password'/>
+          <small>Password must be at least 8 charecters</small>
+        </div>
+
+        {/* #4 : Confirm Password */}
+        <div className='form-input'>
+          <label>Confirm-password</label>
+          <input type='password'/>
+          <small>Password does not match!</small>
+        </div>
+        <button type='submit'>Register</button>
+      </form>
+      </div>
+};
+
+//Render
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<Form/>);
